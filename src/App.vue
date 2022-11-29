b36acb0adeeb0f22933512d030587c9ba5cd4ef3
amakhlouf <33183806+amakhlouf@users.noreply.github.com>
2022-11-29 11:18:53 +0200

    added fetchTask & fetchTasks

--- src/App.vue
+++ src/App.vue
@@ -51,28 +51,19 @@
 					(task) => task.id === id ? {...task, reminder: !task.reminder} : task
 				)
 			},
+			async fetchTasks() {
+				const res = await fetch('http://localhost:5000/tasks')
+				const data = await res.json()
+				return data
+			},
+			async fetchTask(id) {
+				const res = await fetch(`http://localhost:5000/tasks/${id}`)
+				const data = await res.json()
+				return data
+			}
 		},
-		created() {
-			this.tasks = [
-			{
-					id: 1,
-					text: 'Dr App',
-					day: 'Mar 1 @ 230',
-					reminder: true,
-				},
-				{
-					id: 2,
-					text: 'Meeting',
-					day: 'Mar 3 @ 230',
-					reminder: true,
-				},
-				{
-					id: 3,
-					text: 'Shopping',
-					day: 'Mar 3 @ 1130',
-					reminder: false,
-				},
-			]
+		async created() {
+			this.tasks = await this.fetchTasks()
 		}
 	}
 </script>
